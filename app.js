650598ab400c9c280bed4230a42e7bb9df11f0e3
Z-Ahmad <zakiahmad42661@gmail.com>
2021-08-30 16:56:49 -0400

    started image upload implementation

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require("express")
-const path = require("path")
-const mongoose = require("mongoose")
-const ejsMate = require("ejs-mate")
-const session = require("express-session")
-const flash = require("connect-flash")
-const ExpressError = require("./utils/ExpressError")
-const methodOverride = require("method-override")
-const passport = require("passport")
-const LocalStrategy = require("passport-local")
-const User = require("./models/user")
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const userRoutes = require("./routes/users")
-const campgroundRoutes = require("./routes/campgrounds")
-const reviewRoutes = require("./routes/reviews")
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
@@ -22,22 +25,22 @@ mongoose
     useFindAndModify: false
   })
   .then(() => {
-    console.log("CONNECTION OPEN!!")
+    console.log("CONNECTION OPEN!!");
   })
   .catch((err) => {
-    console.log("OH NO ERROR!!")
-    console.log(err)
-  })
+    console.log("OH NO ERROR!!");
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
 
-app.engine("ejs", ejsMate)
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 const sessionConfig = {
   secret: "secret",
   resave: false,
@@ -47,43 +50,43 @@ const sessionConfig = {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
-}
+};
 
-app.use(session(sessionConfig))
-app.use(flash())
-passport.use(new LocalStrategy(User.authenticate()))
+app.use(session(sessionConfig));
+app.use(flash());
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user
-  res.locals.success = req.flash("success")
-  res.locals.error = req.flash("error")
-  next()
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-app.use("/", userRoutes)
-app.use("/campgrounds", campgroundRoutes)
-app.use("/campgrounds/:id/reviews", reviewRoutes)
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
-  res.redirect("/campgrounds")
-})
+  res.redirect("/campgrounds");
+});
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page Not Found", 404))
-})
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err
-  if (!err.message) err.message = "Oh No, Something Went Wrong!"
-  res.status(statusCode).render("error", { err })
-})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(3000, () => {
-  console.log("Serving on port 3000")
-})
+  console.log("Serving on port 3000");
+});
