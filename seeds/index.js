ab4e2760193287a5af84b68b0d3400b3381e972f
Z-Ahmad <zakiahmad42661@gmail.com>
2021-09-18 15:32:56 -0400

    added cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -31,19 +31,19 @@ const seedDB = async () => {
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
-      geometry: { type: "Point", coordinates: [-113.1331, 47.0202] },
+      geometry: { type: "Point", coordinates: [cities[random1000].longitude, cities[random1000].latitude] },
       images: [
         {
-          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1630953997/YelpCamp/iegwzf1nieeqf3ns24un.jpg",
-          filename: "YelpCamp/iegwzf1nieeqf3ns24un"
+          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1631028789/YelpCamp/z4zcmc4nbiug5rez4a75.jpg",
+          filename: "YelpCamp/z4zcmc4nbiug5rez4a75"
         },
         {
-          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1630953997/YelpCamp/weojkcuvltx2mpjq2syc.jpg",
-          filename: "YelpCamp/weojkcuvltx2mpjq2syc"
+          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1631323386/YelpCamp/iqfr5g59vrmpbwwohsad.jpg",
+          filename: "YelpCamp/iqfr5g59vrmpbwwohsad"
         },
         {
-          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1630953997/YelpCamp/ppbvno2msgiuuodwkrqa.jpg",
-          filename: "YelpCamp/ppbvno2msgiuuodwkrqa"
+          url: "https://res.cloudinary.com/dcn6awac0/image/upload/v1631028744/YelpCamp/bvwa6ykxdgmulazt3ine.jpg",
+          filename: "YelpCamp/bvwa6ykxdgmulazt3ine"
         }
       ]
     });
